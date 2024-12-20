83ce5297659af5c36cee3739fe3fcb6966797cca
Technocrat-dev <its.manas.sharma@gmail.com>
2024-12-20 13:41:21 +0530

    adding a new file

---
+++ prog1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int x=0;
+    int y=0;
+    
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        if(s[i] >= 97) {
+            y++;
+        }
+        else {x++;}
+    }
+    if (x>y) {
+        for (int i = 0; i < s.length(); i++) {
+            if(s[i] >= 97) {
+                s[i] -= 32;
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < s.length(); i++) {
+            if(s[i] < 97) {
+                s[i] += 32;
+            }
+        }
+    }
+    cout << s;
+    return 0;
+}
